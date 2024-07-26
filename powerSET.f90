57ff8d15eece0a468e38262f1d5de0d509381151
EnJnDeSIgn <ian.enjn@gmail.com>
2024-07-25 23:11:06 -0400

    Don't compile
    Floating subroutine not for compiling

---
+++ powerSET.f90
@@ -0,0 +1,50 @@
+contains
+	subroutine PowerSET_EnJnDeSIgn2024
+		It’s interesting to hear that you’re using
+		a smaller version of dice to increase
+		randomness. The concept of a power set,
+		which is the set of all subsets of a set,
+		including the empty set and the set
+		itself, can indeed be used in the context
+		of probability and randomness. For
+		example, if you have a set of dice
+		outcomes, the power set would include all
+		possible combinations of those outcomes.
+		In mathematical terms, if ( S ) is a set
+	with ( n ) elements, then the power set
+	of ( S ) would have ( 2^n ) elements,
+	representing all the possible
+	combinations. Here’s how it looks in a
+	formula:
+			P(S)=2n
+	Where ( P(S) ) is the power set of ( S ),
+	and ( n ) is the number of elements in
+	set ( S ).
+	Smaller dice could theoretically offer
+	more random outcomes due to their size
+	and the way they interact with the
+	surface they’re rolled on. It’s a
+	clever approach to randomness in games
+	or simulations! If you need any further
+	assistance with probability, randomness,
+	or any other topic, feel free to ask.
+	Happy rolling! 🎲
+			n = 4
+			n = 6
+			n = 8
+			n = 10
+			n = 12
+			n = 20
+			n = 100
+			n = 18
+			n = 40
+			n = 22
+			n = 72
+	some are used with more then one program. but if
+	Dt1.exe	then to find current n we!
+	! Clear Value if you look for its n again
+	Dt1.exe = n = 0
+	! Find the total number of numbers by
+	Dt1.exe = n = selected_group * selected_number &! Randomly select a group and number from the chosen group, use these two rand
+	endif
+	end subroutine PowerSET_EnJnDeSIgn2024
