ebe70603fbcb4b95def2257db057b25ffa36ff74
Ian <ian.enjn@gmail.com>
2024-09-28 00:58:18 -0400

    Add files via upload
    new project

---
+++ bc.f90
@@ -0,0 +1,90 @@
+program bell_curve
+! In statistics, the bell curve is often associated with the
+! normal distribution, which is a probability distribution
+! that is symmetric about the mean, showing that data near
+! the mean are more frequent in occurrence than data far from
+! the mean. The equation for the normal distribution is:
+! f(x)=σ2π​1​e−21​(σx−μ​)2
+! Here’s what each symbol represents:
+
+! ( f(x) ): Probability density function (pdf)
+! ( x ): The variable
+! ( \mu ): Mean of the distribution
+! ( \sigma ): Standard deviation of the distribution
+! ( \pi ): Mathematical constant Pi (approximately 3.14159)
+! ( e ): Euler’s number (approximately 2.71828)
+
+! The normal distribution is characterized by the mean and
+! the standard deviation. The mean determines the location of
+! the center of the graph, and the standard deviation
+! determines the height and width of the graph
+! When comparing two bell curves (normal distributions), you
+! would look at their means and standard deviations. If the
+! means are different, the curves will be centered at
+! different points on the x-axis. If the standard deviations
+! are different, one curve will be wider or narrower than the
+! other
+! To compare the distributions quantitatively, statisticians
+! often use z-scores, which measure the distance of a score
+! from the mean in units of standard deviation. This allows
+! for comparison between different normal distributions
+end program bell_curve
+
+       
+      
+      
+        
+      
+    
+  
+
+  
+  
+  
+  
+  
+  
+  
+    
+    
+  
+
+    
+     
+  
+
+         
+        
+    
+    
+  
+
+    
+    
+    
+    
+    
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
