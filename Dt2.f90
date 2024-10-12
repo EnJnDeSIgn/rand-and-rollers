eb68d83f8b0b22892af861febfb54d4d829ae058
Ian <ian.enjn@gmail.com>
2024-10-11 21:08:44 -0400

    Add files via upload
    cleaned up group Initialized and groups array!

--- Dt2.f90
+++ Dt2.f90
@@ -2,8 +2,7 @@ program Dt2_roller_EnJnDeSIgn2024
     implicit none
     integer :: selected_group, selected_number, i, j, k, roll_count, num_iterations, count, max_digit, max_digit_count
     character(len=4), dimension(0:9, 0:9) :: groups
-    character(len=4), dimension(10) :: group0, group1, group2, group3, group4, group5, group6, group7, group8, &
-                                        &group9
+    character(len=4), dimension(10) :: group0, group1, group2, group3, group4, group5, group6, group7, group8, group9
     character(len=1), dimension(30) :: selected_numbers
     integer, dimension(30) :: final_numbers, digit_count
 	character(len=50) :: most_frequent_digits
@@ -23,48 +22,19 @@ program Dt2_roller_EnJnDeSIgn2024
 	carry_over = 0.0
 	digit_count = 0
 
-    ! Initialize group0
-    group0 = (/ "1  ", "1  ", "1  ", "1  ", "1  ", &
-                "1  ", "0  ", "0  ", "0  ", "0  " /)
-    ! Initialize group1
-    group1 = (/ "1  ", "0  ", "1  ", "0  ", "0  ", &
-                "1  ", "1  ", "1  ", "1  ", "0  " /)
-    ! Initialize group2
-    group2 = (/ "1  ", "1  ", "0  ", "0  ", "1  ", &
-                "0  ", "1  ", "0  ", "0  ", "0  " /)
-    ! Initialize group3
-    group3 = (/ "1  ", "0  ", "0  ", "1  ", "0  ", &
-                "1  ", "0  ", "0  ", "1  ", "1  " /)
-    ! Initialize group4
-    group4 = (/ "0  ", "1  ", "1  ", "0  ", "0  ", &
-                "0  ", "1  ", "1  ", "0  ", "0  " /)
-    ! Initialize group5
-    group5 = (/ "1  ", "1  ", "1  ", "0  ", "1  ", &
-                "1  ", "1  ", "0  ", "0  ", "1  " /)
-    ! Initialize group6
-    group6 = (/ "0  ", "1  ", "0  ", "1  ", "1  ", &
-                "1  ", "0  ", "1  ", "1  ", "0  " /)
-    ! Initialize group7
-    group7 = (/ "1  ", "1  ", "0  ", "0  ", "0  ", &
-                "1  ", "1  ", "1  ", "0  ", "0  " /)
-    ! Initialize group8
-    group8 = (/ "1  ", "0  ", "0  ", "0  ", "0  ", &
-                "0  ", "0  ", "1  ", "0  ", "0  " /)
-    ! Initialize group9
-    group9 = (/ "0  ", "1  ", "1  ", "1  ", "0  ", &
-                "0  ", "1  ", "0  ", "0  ", "1  "/)
-
-    ! Assign groups to the groups array
-    groups(0, :) = group0
-    groups(1, :) = group1
-    groups(2, :) = group2
-    groups(3, :) = group3
-    groups(4, :) = group4
-    groups(5, :) = group5
-    groups(6, :) = group6
-    groups(7, :) = group7
-    groups(8, :) = group8
-    groups(9, :) = group9
+    ! Initialize group arrays (same as before)
+    group0 = (/ "1  ", "1  ", "1  ", "1  ", "1  ", "1  ", "0  ", "0  ", "0  ", "0  " /)
+    group1 = (/ "1  ", "0  ", "1  ", "0  ", "0  ", "1  ", "1  ", "1  ", "1  ", "0  " /)
+    group2 = (/ "1  ", "1  ", "0  ", "0  ", "1  ", "0  ", "1  ", "0  ", "0  ", "0  " /)
+    group3 = (/ "1  ", "0  ", "0  ", "1  ", "0  ", "1  ", "0  ", "0  ", "1  ", "1  " /)
+    group4 = (/ "0  ", "1  ", "1  ", "0  ", "0  ", "0  ", "1  ", "1  ", "0  ", "0  " /)
+    group5 = (/ "1  ", "1  ", "1  ", "0  ", "1  ", "1  ", "1  ", "0  ", "0  ", "1  " /)
+    group6 = (/ "0  ", "1  ", "0  ", "1  ", "1  ", "1  ", "0  ", "1  ", "1  ", "0  " /)
+    group7 = (/ "1  ", "1  ", "0  ", "0  ", "0  ", "1  ", "1  ", "1  ", "0  ", "0  " /)
+    group8 = (/ "1  ", "0  ", "0  ", "0  ", "0  ", "0  ", "0  ", "1  ", "0  ", "0  " /)
+    group9 = (/ "0  ", "1  ", "1  ", "1  ", "0  ", "0  ", "1  ", "0  ", "0  ", "1  "/)
+    groups(0,:) = group0; groups(1,:) = group1; groups(2,:) = group2; groups(3,:) = group3; groups(4,:) = group4
+    groups(5,:) = group5; groups(6,:) = group6; groups(7,:) = group7; groups(8,:) = group8; groups(9,:) = group9
 
     do j = 1, num_iterations
         random_number_str = ""
