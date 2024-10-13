ce41f6d5d5e1ddf25939b10319fd6716f1679120
Ian <ian.enjn@gmail.com>
2024-10-13 01:09:03 -0400

    Add files via upload
    New random locations for the 0-1's

--- Die20.f90
+++ Die20.f90
@@ -22,48 +22,19 @@ program Die20_roller_EnJnDeSIgn2024
 	carry_over = 0.0
 	digit_count = 0
 
-    ! Initialize group0
-    group0 = (/ "1  ", "1  ", "1  ", "1  ", "1  ", &
-                "1  ", "0  ", "0  ", "0  ", "0  " /)
-    ! Initialize group1
-    group1 = (/ "1  ", "0  ", "1  ", "0  ", "0  ", &
-                "1  ", "1  ", "1  ", "1  ", "0  " /)
-    ! Initialize group2
-    group2 = (/ "1  ", "1  ", "0  ", "0  ", "1  ", &
-                "0  ", "1  ", "0  ", "0  ", "0  " /)
-    ! Initialize group3
-    group3 = (/ "1  ", "0  ", "0  ", "1  ", "0  ", &
-                "1  ", "0  ", "0  ", "1  ", "1  " /)
-    ! Initialize group4
-    group4 = (/ "0  ", "1  ", "1  ", "0  ", "0  ", &
-                "0  ", "1  ", "1  ", "0  ", "0  " /)
-    ! Initialize group5
-    group5 = (/ "1  ", "1  ", "1  ", "0  ", "1  ", &
-                "1  ", "1  ", "0  ", "0  ", "1  " /)
-    ! Initialize group6
-    group6 = (/ "0  ", "1  ", "0  ", "1  ", "1  ", &
-                "1  ", "0  ", "1  ", "1  ", "0  " /)
-    ! Initialize group7
-    group7 = (/ "1  ", "1  ", "0  ", "0  ", "0  ", &
-                "1  ", "1  ", "1  ", "0  ", "0  " /)
-    ! Initialize group8
-    group8 = (/ "1  ", "0  ", "0  ", "0  ", "0  ", &
-                "0  ", "0  ", "1  ", "0  ", "0  " /)
-    ! Initialize group9
-    group9 = (/ "0  ", "1  ", "1  ", "1  ", "0  ", &
-                "0  ", "1  ", "0  ", "0  ", "1  "/)
-
-    ! Assign groups to the groups array
-    groups(0, :) = group0
-    groups(1, :) = group1
-    groups(2, :) = group2
-    groups(3, :) = group3
-    groups(4, :) = group4
-    groups(5, :) = group5
-    groups(6, :) = group6
-    groups(7, :) = group7
-    groups(8, :) = group8
-    groups(9, :) = group9
+    ! Initialize group arrays (same as before)
+    group0 = (/ "0  ", "1  ", "0  ", "0  ", "0  ", "0  ", "0  ", "0  ", "0  ", "1  " /)
+    group1 = (/ "0  ", "1  ", "1  ", "1  ", "1  ", "1  ", "1  ", "0  ", "0  ", "0  " /)
+    group2 = (/ "1  ", "0  ", "0  ", "1  ", "1  ", "0  ", "0  ", "1  ", "1  ", "0  " /)
+    group3 = (/ "1  ", "1  ", "0  ", "1  ", "1  ", "0  ", "0  ", "0  ", "0  ", "0  " /)
+    group4 = (/ "0  ", "1  ", "0  ", "1  ", "1  ", "1  ", "1  ", "1  ", "0  ", "0  " /)
+    group5 = (/ "0  ", "0  ", "1  ", "1  ", "0  ", "0  ", "1  ", "1  ", "1  ", "0  " /)
+    group6 = (/ "1  ", "1  ", "1  ", "1  ", "0  ", "0  ", "1  ", "1  ", "0  ", "0  " /)
+    group7 = (/ "1  ", "1  ", "0  ", "1  ", "0  ", "1  ", "1  ", "1  ", "0  ", "0  " /)
+    group8 = (/ "0  ", "0  ", "1  ", "0  ", "1  ", "0  ", "0  ", "1  ", "1  ", "1  " /)
+    group9 = (/ "0  ", "1  ", "1  ", "0  ", "0  ", "1  ", "0  ", "0  ", "1  ", "1  "/)
+    groups(0,:) = group0; groups(1,:) = group1; groups(2,:) = group2; groups(3,:) = group3; groups(4,:) = group4
+    groups(5,:) = group5; groups(6,:) = group6; groups(7,:) = group7; groups(8,:) = group8; groups(9,:) = group9
 
     do j = 1, num_iterations
         random_number_str = ""
