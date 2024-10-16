7e0d3fd2ba3620515e03148d84b4b3c82bfc1788
Ian <ian.enjn@gmail.com>
2024-10-15 21:30:12 -0400

    Add files via upload
    Fixed the 2ed option from a 2ed 0 to a 1

--- DvVv8.f90
+++ DvVv8.f90
@@ -224,7 +224,7 @@ program DvVv8_roller_EnJnDeSIgn2024
 					else if (random_val2 == 1) then
 					exponent = exponent / total_sum		! This one's for - Exponent's. 1
 					end if
-				else if (random_val3 == 0) then
+				else if (random_val3 == 1) then
 					if (random_val2 == 0) then
 					exponent = exponent / total_sum
 					else if (random_val2 == 1) then
