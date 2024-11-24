3dc39476c229816ac1bdf31b32c7c80e45289f89
Ian <ian.enjn@gmail.com>
2024-11-24 13:20:18 -0500

    Add files via upload
    Changed seed gen code

--- Dt7.f90
+++ Dt7.f90
@@ -28,12 +28,12 @@ end module PowerSetModule_EnJnDeSIgn2024_Dt7
 program Dt7
 	use PowerSetModule_EnJnDeSIgn2024_Dt7
 	implicit none
-	integer :: num0, num1, num2, n, total_subsets, i, num3, num4, num5
+	integer :: num0, num1, num2, n, total_subsets, i, num3, num4, num5, num6, num7, num8
 	integer, allocatable :: base_set(:), seed(:)
 	integer, allocatable :: subsets(:, :)
 	real :: rand
-	character(len=10) :: num0_str, num1_str, num2_str, num3_str, num4_str, num5_str
-	character(len=30) :: combined_str0, combined_str1
+	character(len=10) :: num0_str, num1_str, num2_str, num3_str, num4_str, num5_str, num6_str, num7_str, num8_str
+	character(len=30) :: combined_str0, combined_str1, combined_str2
 	integer :: clock_seed(8)
 	
 	call date_and_time(values=clock_seed)
@@ -55,8 +55,13 @@ program Dt7
 	total_subsets = size(subsets, 1)
     allocate(seed(1:n))	
 	do i = 1, n
+		seed(i) = 0
 		call random_number(rand)
-		seed(i) = mod(int(rand * total_subsets) + 1, 2147483647)  ! Use varied elements, ensure fit within seed range
+		seed(i) = mod(int(rand * 1000000000), 1000000000)
+		! Ensure within 0-4 range
+		!if (seed(i) > 4 .or. seed(i) < 0) then
+			!seed(i) = 0
+		!end if
 	end do
 	
 	call random_number(rand)
@@ -84,10 +89,24 @@ program Dt7
 	write(num4_str, '(I10)') num4
 	write(num5_str, '(I10)') num5
 	
+	call random_number(rand)
+	num6 = int(rand * 1000000000)
+	call random_number(rand)
+	num7 = int(rand * 1000000000)
+	call random_number(rand)
+	num8 = int(rand * 1000000000)
+	
+	write(num6_str, '(I10)') num6
+	write(num7_str, '(I10)') num7
+	write(num8_str, '(I10)') num8
+	
 	! Combine strings to form a 30-digit number 
 	combined_str0 = trim(adjustl(num0_str)) // trim(adjustl(num1_str)) // trim(adjustl(num2_str))
 	!print *, num0, num1, num2
 	combined_str1 = trim(adjustl(num3_str)) // trim(adjustl(num4_str)) // trim(adjustl(num5_str))
+	!!!!
+	combined_str2 = trim(adjustl(num6_str)) // trim(adjustl(num7_str)) // trim(adjustl(num8_str))
 	print *, combined_str0
 	print *, combined_str1
+	print *, combined_str2
 end program Dt7
