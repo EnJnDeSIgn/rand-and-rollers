40fa15484077c9605189050704098f233f7a7b5b
Ian <ian.enjn@gmail.com>
2024-11-25 11:08:05 -0500

    Add files via upload
    Added mod code language to number calls

--- Dt7.f90
+++ Dt7.f90
@@ -29,15 +29,15 @@ program Dt7
 	use PowerSetModule_EnJnDeSIgn2024_Dt7
 	implicit none
 	integer :: num0, num1, num2, n, total_subsets, i, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12, num13, &
-	num14, num15, num16, num17, num18, num19, num20, num21, num22, num23
+	num14, num15, num16, num17, num18, num19, num20, num21, num22, num23, num24, num25, num26, num27, num28, num29
 	integer, allocatable :: base_set(:), seed(:)
 	integer, allocatable :: subsets(:, :)
 	real :: rand
 	character(len=10) :: num0_str, num1_str, num2_str, num3_str, num4_str, num5_str, num6_str, num7_str, num8_str, num9_str, &
 	num10_str, num11_str, num12_str, num13_str, num14_str, num15_str, num16_str, num17_str, num18_str, num19_str, num20_str, &
-	num21_str, num22_str, num23_str
+	num21_str, num22_str, num23_str, num24_str, num25_str, num26_str, num27_str, num28_str, num29_str
 	character(len=30) :: combined_str0, combined_str1, combined_str2, combined_str3, combined_str4, combined_str5, combined_str6, &
-	combined_str7
+	combined_str7, combined_str8, combined_str9
 	integer :: clock_seed(8)
 	
 	call date_and_time(values=clock_seed)
@@ -69,95 +69,144 @@ program Dt7
 	end do
 	
 	call random_number(rand)
-	num0 = int(rand * 1000000000)
-	
+	!num0 = int(rand * 1000000000)
+	num0 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num1 = int(rand * 1000000000)
-	
+	!num1 = int(rand * 1000000000)
+	num1 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num2 = int(rand * 1000000000)
-	
+	!num2 = int(rand * 1000000000)
+	num2 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	! Convert numbers to strings
 	write(num0_str, '(I10)') num0 
 	write(num1_str, '(I10)') num1 
 	write(num2_str, '(I10)') num2
 	
 	call random_number(rand)
-	num3 = int(rand * 1000000000)
+	!num3 = int(rand * 1000000000)
+	num3 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num4 = int(rand * 1000000000)
+	!num4 = int(rand * 1000000000)
+	num4 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num5 = int(rand * 1000000000)
+	!num5 = int(rand * 1000000000)
+	num5 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num3_str, '(I10)') num3
 	write(num4_str, '(I10)') num4
 	write(num5_str, '(I10)') num5
 	
 	call random_number(rand)
-	num6 = int(rand * 1000000000)
+	!num6 = int(rand * 1000000000)
+	num6 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num7 = int(rand * 1000000000)
+	!num7 = int(rand * 1000000000)
+	num7 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num8 = int(rand * 1000000000)
+	!num8 = int(rand * 1000000000)
+	num8 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num6_str, '(I10)') num6
 	write(num7_str, '(I10)') num7
 	write(num8_str, '(I10)') num8
 	
 	call random_number(rand)
-	num9 = int(rand * 1000000000)
+	!num9 = int(rand * 1000000000)
+	num9 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num10 = int(rand * 1000000000)
+	!num10 = int(rand * 1000000000)
+	num10 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num11 = int(rand * 1000000000)
+	!num11 = int(rand * 1000000000)
+	num11 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num9_str, '(I10)') num9
 	write(num10_str, '(I10)') num10
 	write(num11_str, '(I10)') num11
 	
 	call random_number(rand)
-	num12 = int(rand * 1000000000)
+	!num12 = int(rand * 1000000000)
+	num12 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num13 = int(rand * 1000000000)
+	!num13 = int(rand * 1000000000)
+	num13 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num14 = int(rand * 1000000000)
+	!num14 = int(rand * 1000000000)
+	num14 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num12_str, '(I10)') num12
 	write(num13_str, '(I10)') num13
 	write(num14_str, '(I10)') num14
 	
 	call random_number(rand)
-	num15 = int(rand * 1000000000)
+	!num15 = int(rand * 1000000000)
+	num15 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num16 = int(rand * 1000000000)
+	!num16 = int(rand * 1000000000)
+	num16 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num17 = int(rand * 1000000000)
+	!num17 = int(rand * 1000000000)
+	num17 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num15_str, '(I10)') num15
 	write(num16_str, '(I10)') num16
 	write(num17_str, '(I10)') num17
 	
 	call random_number(rand)
-	num18 = int(rand * 1000000000)
+	!num18 = int(rand * 1000000000)
+	num18 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num19 = int(rand * 1000000000)
+	!num19 = int(rand * 1000000000)
+	num19 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num20 = int(rand * 1000000000)
+	!num20 = int(rand * 1000000000)
+	num20 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num18_str, '(I10)') num18
 	write(num19_str, '(I10)') num19
 	write(num20_str, '(I10)') num20
 	
 	call random_number(rand)
-	num21 = int(rand * 1000000000)
+	!num21 = int(rand * 1000000000)
+	num21 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num22 = int(rand * 1000000000)
+	!num22 = int(rand * 1000000000)
+	num22 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	call random_number(rand)
-	num23 = int(rand * 1000000000)
+	!num23 = int(rand * 1000000000)
+	num23 = mod(seed(i) + int(rand * 1000000000), 1000000000)
 	
 	write(num21_str, '(I10)') num21
 	write(num22_str, '(I10)') num22
 	write(num23_str, '(I10)') num23
+	
+	call random_number(rand)
+	!num24 = int(rand * 1000000000)
+	num24 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	call random_number(rand)
+	!num25 = int(rand * 1000000000)
+	num25 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	call random_number(rand)
+	!num26 = int(rand * 1000000000)
+	num26 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	
+	write(num24_str, '(I10)') num24
+	write(num25_str, '(I10)') num25
+	write(num26_str, '(I10)') num26
+	
+	call random_number(rand)
+	!num27 = int(rand * 1000000000)
+	num27 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	call random_number(rand)
+	!num28 = int(rand * 1000000000)
+	num28 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	call random_number(rand)
+	!num29 = int(rand * 1000000000)
+	num29 = mod(seed(i) + int(rand * 1000000000), 1000000000)
+	
+	write(num27_str, '(I10)') num27
+	write(num28_str, '(I10)') num28
+	write(num29_str, '(I10)') num29
 	
 	! Combine strings to form a 30-digit number 
 	combined_str0 = trim(adjustl(num0_str)) // trim(adjustl(num1_str)) // trim(adjustl(num2_str))
@@ -175,6 +224,10 @@ program Dt7
 	combined_str6 = trim(adjustl(num18_str)) // trim(adjustl(num19_str)) // trim(adjustl(num20_str))
 	!!!!
 	combined_str7 = trim(adjustl(num21_str)) // trim(adjustl(num22_str)) // trim(adjustl(num23_str))
+	!!!!
+	combined_str8 = trim(adjustl(num24_str)) // trim(adjustl(num25_str)) // trim(adjustl(num26_str))
+	!!!!
+	combined_str9 = trim(adjustl(num27_str)) // trim(adjustl(num28_str)) // trim(adjustl(num29_str))
 	print *, combined_str0
 	print *, combined_str1
 	print *, combined_str2
@@ -183,4 +236,6 @@ program Dt7
 	print *, combined_str5
 	print *, combined_str6
 	print *, combined_str7
+	print *, combined_str8
+	print *, combined_str9
 end program Dt7
