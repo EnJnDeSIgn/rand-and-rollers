ef457413fc34d883fb20f636706e3e874968d8fc
Ian <ian.enjn@gmail.com>
2024-11-30 15:57:54 -0500

    Add files via upload
    unworked as of yet except ian'swordeq.txt file with copilot's python

---
+++ newtonsM.f90
@@ -0,0 +1,17 @@
+! Steps to Implement Newton's Method
+! Define the Function and its Derivative: You need the function for which you want to find the root and its derivative.
+
+! Initial Guess: Start with an initial guess of the root.
+
+! Iterative Formula: Use the formula to find a better approximation:
+
+𝑥𝑛+1=𝑥𝑛−𝑓(𝑥𝑛) / 𝑓′(𝑥𝑛)
+! Repeat this step until convergence.
+
+! Example: Find the Root of 
+𝑓(𝑥)=𝑥^3−2𝑥^2+𝑥−3
+! Function: 
+𝑓(𝑥)=𝑥^3−2𝑥^2+𝑥−3
+
+! Derivative: 
+𝑓′(𝑥)=3𝑥^2−4𝑥+1
