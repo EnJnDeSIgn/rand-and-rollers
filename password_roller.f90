4e17e25940e3cae7dd877eac1a659a8140ad6d89
Ian <ian.enjn@gmail.com>
2024-12-19 10:30:28 -0500

    Add files via upload
    Added group with missing capitals' and matching number of 0's too other numbers

--- password_roller.f90
+++ password_roller.f90
@@ -2,9 +2,9 @@ program password_roller_EnJnDeSIgn2024
 	implicit none
 	character(len=1) :: user_input
 	integer :: selected_group, selected_number, i, count, j
-	character(len=2), dimension(0:13, 0:8) :: groups
+	character(len=2), dimension(0:14, 0:8) :: groups
 	character(len=2), dimension(9) :: group0, group1, group2, group3, group4, group5, group6, group7, group8, group9, group10
-	character(len=2), dimension(9) :: group11, group12, group13
+	character(len=2), dimension(9) :: group11, group12, group13, group14
 	character(len=2) :: element
 	character(len=255) :: result_string, all_results
 	integer :: number, total, ios
@@ -26,20 +26,21 @@ program password_roller_EnJnDeSIgn2024
 	group0 = (/ "i ", "+ ", "5 ", "8 ", "a ", "p ", "4 ", "r ", "/ "/)	! 9
 	group1 = (/ "h ", "J ", "3 ", "% ", "H ", "| ", "7 ", "t ", "= "/)	! 18
 	group2 = (/ "@ ", "x ", "! ", "& ", "I ", "9 ", "7 ", "# ", "3 "/)	! 27
-	group3 = (/ "g ", "2 ", "A ", "3 ", "/ ", "c ", "R ", "0 ", "y "/)	! 36
-	group4 = (/ "9 ", "e ", "= ", "V ", "j ", "s ", "u ", "? ", "+ "/)	! 45
-	group5 = (/ "7 ", ". ", ". ", "1 ", "< ", "4 ", "z ", "U ", "- "/)	! 54
-	group6 = (/ "6 ", "5 ", "> ", "T ", "v ", "n ", "4 ", "Q ", "< "/)	! 63
-	group7 = (/ "2 ", "@ ", "5 ", "9 ", "d ", "! ", "3 ", "W ", "b "/)	! 72
+	group3 = (/ "L ", "2 ", "A ", "3 ", "/ ", "c ", "R ", "0 ", "y "/)	! 36
+	group4 = (/ "9 ", "e ", "= ", "V ", "j ", "s ", "u ", "? ", " 0"/)	! 45
+	group5 = (/ "7 ", ". ", ". ", "1 ", "P ", "4 ", "z ", "U ", "- "/)	! 54
+	group6 = (/ "6 ", "5 ", "K ", "T ", "v ", "n ", "4 ", "M ", "< "/)	! 63
+	group7 = (/ "2 ", "N ", "5 ", "9 ", "d ", "! ", "3 ", "W ", "b "/)	! 72
 	group8 = (/ "9 ", "6 ", "& ", ", ", "C ", "X ", ", ", "B ", "# "/)	! 81
 	group9 = (/ "$ ", "7 ", "8 ", "8 ", "1 ", "S ", "9 ", "* ", "o "/)	! 90
-   group10 = (/ "6 ", "Z ", "? ", "7 ", "6 ", "5 ", "1 ", "8 ", "2 "/)	! 99
-   group11 = (/ "2 ", "w ", "8 ", "% ", "G ", "F ", "E ", "D ", "k "/)	! 108
-   group12 = (/ "f ", "1 ", "4 ", "4 ", "* ", "q ", "3 ", "> ", "2 "/)
+   group10 = (/ "6 ", "Z ", "? ", "7 ", "6 ", "5 ", "1 ", "8 ", "2 "/)	! 99 a b c d e f g h i j k l m n o p q r s t u v w x y z
+   group11 = (/ "2 ", "w ", "8 ", "% ", "G ", "F ", "E ", " 0", "k "/)	! 108
+   group12 = (/ " O", "1 ", "4 ", "4 ", "* ", "q ", "3 ", "> ", "2 "/)
    group13 = (/ "5 ", "l ", "m ", "| ", "- ", "1 ", "6 ", "0 ", "Y "/)
+   group14 = (/ "Q ", "0 ", "+ ", "> ", "D ", "g ", "@ ", "< ", "f "/)
 	groups(0,:) = group0; groups(1,:) = group1; groups(2,:) = group2; groups(3,:) = group3; groups(4,:) = group4
     groups(5,:) = group5; groups(6,:) = group6; groups(7,:) = group7; groups(8,:) = group8; groups(9,:) = group9
-	groups(10,:) = group10; groups(11,:) = group11; groups(12,:) = group12; groups(13,:) = group13
+	groups(10,:) = group10; groups(11,:) = group11; groups(12,:) = group12; groups(13,:) = group13; groups(14,:) = group14
 	
 	all_results = ""
 	count = 0
@@ -53,7 +54,7 @@ do while (.true.)
 	do i = 1, 25
 		! Randomly select a group
 		call random_number(rand)
-		selected_group = int(rand * 14)
+		selected_group = int(rand * 15)
 		! Randomly select a number from the chosen group
 		call random_number(rand)
 		selected_number = int(rand * 9)
