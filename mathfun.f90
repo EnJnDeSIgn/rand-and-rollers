9e4902e37068854ad5f9aac9b7234edc65d71d0e
Ian <ian.enjn@gmail.com>
2025-01-04 18:31:17 -0500

    Add files via upload
    more division and fix's

--- mathfun.f90
+++ mathfun.f90
@@ -11,7 +11,7 @@ program MathFun_EnJnDeSIgn2024
 
     do i = 1, 10  ! 10 problems for the game
 		! Start timer
-        !call cpu_time(start_time)
+        call cpu_time(start_time)
 		! Old call
         !call random_number(rand)
         !num1 = int(rand * 10) + 1  ! Random number between 1 and 10
@@ -28,14 +28,16 @@ program MathFun_EnJnDeSIgn2024
 			else if (random_val1 == 1) then
 			Call random_number(rand)
 			num1 = int(rand * 5) + 6	!6-10
+			else if (random_val1 == 2) then
+			num1 = int(rand * 5) + 11
 			end if
-			if (num1 < 1 .or. num1 > 10) then
+			if (num1 < 1 .or. num1 > 15) then
 					print *, "Error: num1 out of range"
 					stop
 			end if
 			! Quantum Version 1.0
 			call random_number(rand)
-			random_val2 = int(rand * 2)
+			random_val2 = int(rand * 3)
 			! Randomly select a num1
 			if (random_val2 == 0) then
 			call random_number(rand)
@@ -43,8 +45,10 @@ program MathFun_EnJnDeSIgn2024
 			else if (random_val2 == 1) then
 			Call random_number(rand)
 			num2 = int(rand * 5) + 6	!6-10
+			else if (random_val2 == 2) then
+			num2 = int(rand * 5) + 11	!11-15
 			end if
-			if (num2 < 1 .or. num2 > 10) then
+			if (num2 < 1 .or. num2 > 15) then
 					print *, "Error: num2 out of range"
 					stop
 			end if
@@ -74,7 +78,7 @@ program MathFun_EnJnDeSIgn2024
                 correct_answer = num1 / num2
             else
 				call random_number(rand)
-				random_val0 = int(rand * 4)
+				random_val0 = int(rand * 8)
 				if (random_val0 == 0) then
                 num1 = num2 * 2  ! If not, set num2 to 1 to avoid errors
                 correct_answer = num1 / num2
@@ -87,11 +91,23 @@ program MathFun_EnJnDeSIgn2024
 				else if (random_val0 == 3) then
 				num1 = num2 * 5
 				correct_answer = num1 / num2
+				else if (random_val0 == 4) then
+				num1 = num2 * 6
+				correct_answer = num1 / num2
+				else if (random_val0 == 5) then
+				num1 = num2 * 7
+				correct_answer = num1 / num2
+				else if (random_val0 == 6) then
+				num1 = num2 * 8
+				correct_answer = num1 / num2
+				else if (random_val0 == 7) then
+				num1 = num2 * 9
+				correct_answer = num1 / num2
 				end if
             end if
         end if
 		! Start timer
-        call cpu_time(start_time)
+        !call cpu_time(start_time)
 
         print *, "What is ", num1, " ", operator, " ", num2, "?"
 		print *
