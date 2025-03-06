c060bc03b8e7c12904312d5d3ede94186463dfcc
Ian <ian.enjn@gmail.com>
2025-03-05 23:08:53 -0500

    Add files via upload
    Has a before and after randomize now

--- run_elegenv1.bat
+++ run_elegenv1.bat
@@ -1,12 +1,16 @@
 @echo off
 cls
 
-REM Run the call_phard.py script to randomize the seed
-echo Running call_phard.py to randomize the seed...
+REM Run the ele_call.py script to randomize the seed
+echo Running ele_call.py to randomize the seed...
 python ele_call.py
 
 REM Run the ELEgenV1.exe program
 echo Running ELEgenV1.exe...
 ELEgenV1.exe
+
+REM Run the ele_call.py script to keep randmizing the seed
+echo running ele_call.py to keep randomizing the seed...
+python ele_call.py
 
 echo Done.
