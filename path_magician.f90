fe13188a4484ef3007a7668f8d7938defb55d821
Ian <ian.enjn@gmail.com>
2025-11-15 13:48:40 -0500

    Add files via upload
    the very beginning of random expansion of the path I invented in wicca

---
+++ path_magician.f90
@@ -0,0 +1,15 @@
+program path_magician
+    implicit none
+	
+print *
+print *, "in wicca it said that we take things that are harmful and use them in a way that is not"
+print *, "so my path of the magician is all about how i'm going to entertain you not rip you off."
+print *
+print *, "    the pharaoh would ask for a magician and they would bring him a sis par aankh      "
+print *, "                     Man (from the) House (of) Life: sis par aankh                     "
+print *
+print *, "     this software is dedicated to the town of scott lewis that dwell in audible       "
+print *, "                                 and other book stores                                 "
+print *
+
+end program path_magician
